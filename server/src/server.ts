5a6e393dfa767ad50f5488bf02c2770d2f96e5c5
carlosjose2018 <carlosaraujo10jose3000@gmail.com>
2023-01-21 10:38:18 -0300

    backend config

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,27 +1,13 @@
 import Fastify from "fastify";
-import cors from "@fastify/cors"
-import {PrismaClient} from "@prisma/client"
+import cors from "@fastify/cors";
 
+import { appRoutes } from "./routes";
 
 const app = Fastify();
-const prisma = new PrismaClient()
 
-app.register(cors)
+app.register(cors);
+app.register(appRoutes);
 
-
-app.get('/hello',async () =>{
-  const habits = await prisma.habit.findFirst({
-    where:{
-      title:{
-        startsWith:'Beber'
-      }
-    }
-  })
-  return habits
-})
-
-app.listen({
-  port:3333,
-}).then(() =>{
-  console.log("HTTP Server running!")
-})
+app.listen({ port: 3333 }, () => {
+  console.log("HTTP server is running 🚀");
+});
