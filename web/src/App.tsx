351dcdff7304f73935d412cadf733c146d415fac
carlosjose2018 <carlosaraujo10jose3000@gmail.com>
2023-01-21 11:29:51 -0300

    creater component header

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,13 @@
 import './styles/global.css'
-
-//import { Habit } from './components/Habit'
-
-import logoHabits from './assets/logoHabits.svg'
+import {Header}  from './components/Header'
 
 export function App() {
   return (
     <div className='w-screen h-screen flex justify-center items-center'>
       <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
-        <img src={logoHabits} alt="Logo Habits"/>
+      
+        <Header/>
+        
       </div>
     </div>
   )
